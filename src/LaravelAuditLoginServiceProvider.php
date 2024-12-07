5087fe5c67758db9142497e681496b5cd58e19ae
Fikri Mastor <fikrimastor@gmail.com>
2024-12-07 20:02:51 +0800

    feat: adjust the publishing config and migrations

--- src/LaravelAuditLoginServiceProvider.php
+++ src/LaravelAuditLoginServiceProvider.php
@@ -4,6 +4,8 @@ namespace FikriMastor\LaravelAuditLogin;
 
 use FikriMastor\LaravelAuditLogin\Commands\LaravelAuditLoginCommand;
 use FikriMastor\LaravelAuditLogin\Listeners\AuditLoginSubscriber;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Event;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -23,6 +25,8 @@ class LaravelAuditLoginServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasMigration('create_audit_logins_table')
             ->hasCommand(LaravelAuditLoginCommand::class);
+
+        $this->offerPublishing();
     }
 
     public function boot(): void
@@ -30,5 +34,40 @@ class LaravelAuditLoginServiceProvider extends PackageServiceProvider
         parent::boot();
 
         Event::subscribe(AuditLoginSubscriber::class);
+    }
+
+    protected function offerPublishing(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        if (! function_exists('config_path')) {
+            // function not available and 'publish' not relevant in Lumen
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/audit-login.php' => config_path('audit-login.php'),
+        ], 'audit-login-config');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_audit_logins_table.php.stub' => $this->getMigrationFileName('create_audit_logins_table.php'),
+        ], 'audit-login-migrations');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     */
+    protected function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
