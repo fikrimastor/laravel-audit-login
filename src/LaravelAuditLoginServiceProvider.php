3be2938b4e80466b2f01acfc0ced8f0c43c7552e
Fikri Mastor <fikrimastor@gmail.com>
2024-12-07 23:17:29 +0800

    fix: initialize attribute using subscriber

--- src/LaravelAuditLoginServiceProvider.php
+++ src/LaravelAuditLoginServiceProvider.php
@@ -2,17 +2,13 @@
 
 namespace FikriMastor\LaravelAuditLogin;
 
-use FikriMastor\LaravelAuditLogin\Actions\FailedLoginEvent;
-use FikriMastor\LaravelAuditLogin\Actions\LoginEvent;
-use FikriMastor\LaravelAuditLogin\Actions\LogoutEvent;
-use FikriMastor\LaravelAuditLogin\Actions\PasswordResetEvent;
-use FikriMastor\LaravelAuditLogin\Actions\RegisteredEvent;
 use FikriMastor\LaravelAuditLogin\Commands\LaravelAuditLoginCommand;
 use FikriMastor\LaravelAuditLogin\Contracts\FailedLoginEventContract;
 use FikriMastor\LaravelAuditLogin\Contracts\LoginEventContract;
 use FikriMastor\LaravelAuditLogin\Contracts\LogoutEventContract;
 use FikriMastor\LaravelAuditLogin\Contracts\PasswordResetEventContract;
 use FikriMastor\LaravelAuditLogin\Contracts\RegisteredEventContract;
+use FikriMastor\LaravelAuditLogin\Listeners\AuditLoginSubscriber;
 use FikriMastor\LaravelAuditLogin\Listeners\FailedListener;
 use FikriMastor\LaravelAuditLogin\Listeners\LoginListener;
 use FikriMastor\LaravelAuditLogin\Listeners\LogoutListener;
@@ -48,16 +44,21 @@ class LaravelAuditLoginServiceProvider extends PackageServiceProvider
     {
         parent::boot();
 
-        $this->app->bind(LoginEventContract::class, LoginEvent::class);
-        $this->app->bind(LogoutEventContract::class, LogoutEvent::class);
-        $this->app->bind(FailedLoginEventContract::class, FailedLoginEvent::class);
-        $this->app->bind(PasswordResetEventContract::class, PasswordResetEvent::class);
-        $this->app->bind(RegisteredEventContract::class, RegisteredEvent::class);
+        LaravelAuditLogin::recordRegisteredUsing(RegisteredEventContract::class);
+        LaravelAuditLogin::recordLoginUsing(LoginEventContract::class);
+        LaravelAuditLogin::recordFailedLoginUsing(FailedLoginEventContract::class);
+        LaravelAuditLogin::recordLogoutUsing(LogoutEventContract::class);
+        LaravelAuditLogin::recordForgotPasswordUsing(PasswordResetEventContract::class);
 
-        Event::listen(Login::class, LoginListener::class);
-        Event::listen(Logout::class, LogoutListener::class);
-        Event::listen(Failed::class, FailedListener::class);
-        Event::listen(Registered::class, RegisteredListener::class);
-        Event::listen(PasswordReset::class, PasswordResetListener::class);
+        Event::subscribe(new AuditLoginSubscriber([
+            'url' => request()->fullUrl(),
+            'ip_address' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+        ]));
+//        Event::listen(Login::class, LoginListener::class);
+//        Event::listen(Logout::class, LogoutListener::class);
+//        Event::listen(Failed::class, FailedListener::class);
+//        Event::listen(Registered::class, RegisteredListener::class);
+//        Event::listen(PasswordReset::class, PasswordResetListener::class);
     }
 }
