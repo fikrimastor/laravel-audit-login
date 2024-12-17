eba16415e7564489fca2bcf928d9f575831fbd91
Fikri Mastor <fikrimastor@gmail.com>
2024-12-17 09:44:11 +0800

    feat: set minimum testbench version for laravel 10 compatibility #13

--- tests/HasEventLogsTest.php
+++ tests/HasEventLogsTest.php
@@ -3,7 +3,10 @@
 use FikriMastor\AuditLogin\AuditLoginAttribute;
 use FikriMastor\AuditLogin\Enums\EventTypeEnum;
 use FikriMastor\AuditLogin\Tests\TestModels\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Request;
+
+uses(RefreshDatabase::class);
 
 it('can test authenticatable class have authLogs many records with event type login', function () {
     $user = User::firstOrCreate(['email' => TEST_USER_EMAIL]);
